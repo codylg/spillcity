3ea27b9b00db2e7778c4319a3e48ebbecd9a22b4
Cody Gordon <clg@clg.name>
2018-11-20 18:54:46 +1000

    Add footer content

--- footer.php
+++ footer.php
@@ -14,9 +14,31 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<a href="https://www.facebook.com/spillcitybris">
-			<img src="/wp-content/themes/spillcity/media/spill-city-b@2x.png" alt="Spill City" />
-		</a>
+		<div class="colophon">
+			<p>
+				For all bookings, media and general enquires:
+			</p>
+			<p>
+				<a href="mailto:amy-flanagan@spilldistrict.com.au">amy-flanagan@spilldistrict.com.au</a>
+			</p>
+			<p>
+				<a href="tel:+61455174600">+614 5517 4600</a>
+			</p>
+		</div>
+		<div class="spill-logo">
+			<a href="https://www.facebook.com/spillcitybris">
+				<img src="/wp-content/themes/spillcity/media/spill-city-b@2x.png" alt="Spill City" />
+			</a>
+		</div>
+		<div class="logos">
+			<a href="http://thezoo.com.au">
+				<img src="/wp-content/themes/spillcity/media/logo-zoo@2x.png" alt="The Zoo" />
+			</a>
+			<a href="http://spilldistrict.com">
+				<img src="/wp-content/themes/spillcity/media/logo-spill-district@2x.png" alt="Spill District" />
+			</a>
+			<img src="/wp-content/themes/spillcity/media/logo-young-henrys@2x.png" alt="Young Henrys" />
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
